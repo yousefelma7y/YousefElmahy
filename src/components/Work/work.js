cd1c30304e25ad87e7083ba7d7256ee3866cec1d
elmahy <joosubway@gmail.com>
2021-11-23 09:20:56 +0200

    edit

--- src/components/Work/work.js
+++ src/components/Work/work.js
@@ -2,59 +2,59 @@ import React  from  'react';
 import {motion} from 'framer-motion/dist/es/index' ;
 import './work.css';
 
-import {useEffect}  from 'react';
-import { useInView } from 'react-intersection-observer';
-import {useAnimation}  from 'framer-motion';
+// import {useEffect}  from 'react';
+// import { useInView } from 'react-intersection-observer';
+// import {useAnimation}  from 'framer-motion';
 
 const Work  =() => {
 
-    const {ref , inView} = useInView({
-        threshold:1
-    });
+    // const {ref , inView} = useInView({
+    //     threshold:1
+    // });
   
-    const body1 = useAnimation();
-    const body2 = useAnimation();
+    // const body1 = useAnimation();
+    // const body2 = useAnimation();
 
-    useEffect(() => {
-        console.log("use effect hook , inView = ", inView) ;
-         if(inView){
+    // useEffect(() => {
+    //     console.log("use effect hook , inView = ", inView) ;
+    //      if(inView){
             
-            body1.start({
+    //         body1.start({
                 
     
-                x: 0 ,
-                transition:{delay:0.15} 
-              });
+    //             x: 0 ,
+    //             transition:{delay:0.15} 
+    //           });
 
-              body2.start({
+    //           body2.start({
                
-                x:0 ,
-                transition:{delay:0.15}         
-              });
+    //             x:0 ,
+    //             transition:{delay:0.15}         
+    //           });
            
 
-         }
-         if(!inView){
+    //      }
+    //      if(!inView){
            
-            body1.start({
+    //         body1.start({
               
-                x: -30     
-              });
+    //             x: -30     
+    //           });
 
-              body2.start({
+    //           body2.start({
                 
-                x: 30           
-              });
-         }
-    },[inView]);
+    //             x: 30           
+    //           });
+    //      }
+    // },[inView]);
 
         return(
             <div  id="work" data-aos="fade-up"  className="work">
-            <div ref={ref} className="container">
+            <div  className="container">
                 <motion.h2 
                  className="work-title"><span>My</span> Work</motion.h2>
 
-                <motion.div  animate={body1}   whileHover={{scale:1.1}} 
+                <motion.div    whileHover={{scale:1.1}} 
                 className="part first">
                     <i className="icon fa fa-chain fa-2x"></i>
                     <h4 className="part-title">Front End</h4>
@@ -64,7 +64,7 @@ const Work  =() => {
                     </p>
                 </motion.div>
                 
-                <motion.div animate={body2}  whileHover={{scale:1.1}} 
+                <motion.div  whileHover={{scale:1.1}} 
                 className="part">
                     <i className="icon fa fa-bolt fa-2x"></i>
                     <h4 className="part-title">Freelance</h4>
